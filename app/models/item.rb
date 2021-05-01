96356bbee147526a1048a384a6ba288b4c834427
hasemoku <kei5_family@icloud.com>
2021-05-01 18:10:40 +0900

    単体モデルテストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   validates :product_name,  presence: true
   validates :description,   presence: true
   validates :price,         presence: true
+  validates :image,         presence: true
 
   validates :condition_id,        numericality: { other_than: 1 } 
   validates :category_id,         numericality: { other_than: 1 } 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品がうまくいくとき' do
+
+      it '存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it 'product_nameが空では登録できない' do
+        @item.product_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Product name can't be blank"
+      end
+      it 'descriptionが空では登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Description can't be blank"
+      end
+      it 'category_idが---では登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category must be other than 1"
+      end
+      it 'condition_idが---では登録できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition must be other than 1"
+      end
+      it 'shipping_charge_idが---では登録できない' do
+        @item.shipping_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping charge must be other than 1"
+      end
+      it 'delivery_source_idが---では登録できない' do
+        @item.delivery_source_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery source must be other than 1"
+      end
+      it 'days_to_ship_idが---では登録できない' do
+        @item.days_to_ship_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Days to ship must be other than 1"
+      end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+    end
+  end
 end
