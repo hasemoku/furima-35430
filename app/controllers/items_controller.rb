94c77db3cf0b0d9f675d6a2caba08439c25d89e4
hasemoku <kei5_family@icloud.com>
2021-05-02 23:12:17 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:update, :edit]
 
   def index
     @items = Item.order('created_at DESC')
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -22,10 +24,34 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path method: :show
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:product_name, :description, :category_id, :condition_id, :shipping_charge_id, :delivery_source_id,
                                  :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
   end
 end
